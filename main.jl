734fa5170d107b6751507bf37f92384051651df5
michael <michael.groom@sydney.edu.au>
2023-05-04 16:49:20 +1000

    Changed vts write to a vtr file

--- main.jl
+++ main.jl
@@ -18,14 +18,14 @@ x, y, z = readPlot3DGrid(timeStep, grid.Nx, grid.Ny, grid.Nz)
 # Load solution
 Q = readPlot3DSolution(timeStep, grid.Nx, grid.Ny, grid.Nz, input.nVars)
 
-# Write solution to a vts file 
+# Write solution to a vtr file 
 iNVars = input.nVars
 # Check that there are 8, 10 or 12 variables
 if (iNVars == 8) || (iNVars == 10) || (iNVars == 12)
-    # Write solution to a vts file
-    filename = "data/solution_$(rpad(string(round(t, digits=5)), 7, "0")).vts"
+    # Write solution to a vtr file
+    filename = "data/solution_$(rpad(string(round(t, digits=5)), 7, "0")).vtr"
     tStart = report("Writing the solution at time $(rpad(string(round(t, digits=5)), 7, "0"))", 1)
-    WriteVTK.vtk_grid(filename, x, y, z) do vtk
+    WriteVTK.vtk_grid(filename, x[:,1,1], y[1,:,1], z[1,1,:]) do vtk
         vtk["MomentumX"] = Q[:, :, :, 1]
         vtk["MomentumY"] = Q[:, :, :, 2]
         vtk["MomentumZ"] = Q[:, :, :, 3]
