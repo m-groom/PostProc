b83cb90d6bb07027adca14afc23f7296af34f224
michael <michael.groom@sydney.edu.au>
2023-05-04 16:58:29 +1000

    Moved solution write to a function

--- main.jl
+++ main.jl
@@ -15,38 +15,13 @@ t = input.startTime
 timeStep = rpad(string(round(t, digits=8)), 10, "0")
 # Load grid
 x, y, z = readPlot3DGrid(timeStep, grid.Nx, grid.Ny, grid.Nz)
+
+# TODO: wrap this in a loop to read all time steps
+
 # Load solution
 Q = readPlot3DSolution(timeStep, grid.Nx, grid.Ny, grid.Nz, input.nVars)
 
-# Write solution to a vtr file 
-iNVars = input.nVars
-# Check that there are 8, 10 or 12 variables
-if (iNVars == 8) || (iNVars == 10) || (iNVars == 12)
-    # Write solution to a vtr file
-    filename = "data/solution_$(rpad(string(round(t, digits=5)), 7, "0")).vtr"
-    tStart = report("Writing the solution at time $(rpad(string(round(t, digits=5)), 7, "0"))", 1)
-    WriteVTK.vtk_grid(filename, x[:,1,1], y[1,:,1], z[1,1,:]) do vtk
-        vtk["MomentumX"] = Q[:, :, :, 1]
-        vtk["MomentumY"] = Q[:, :, :, 2]
-        vtk["MomentumZ"] = Q[:, :, :, 3]
-        vtk["EnergyDensity"] = Q[:, :, :, 4]
-        if (iNVars >= 10)
-            for v = 1:2
-                vtk["DensityMassFraction$(v)"] = Q[:, :, :, 4+v]
-            end
-        end
-        if (iNVars == 12)
-            for v = 1:2
-                vtk["VolumeFraction$(v)"] = Q[:, :, :, 4+2+v]
-            end
-        end
-        vtk["Density"] = Q[:, :, :, iNVars-3]
-        vtk["Gamma"] = Q[:, :, :, iNVars-2]
-        vtk["Pressure"] = Q[:, :, :, iNVars-1]
-        vtk["Temperature"] = Q[:, :, :, iNVars]
-    end
-else
-    error("Number of variables must be 8, 10 or 12")
-end
-tEnd = report("Finished writing solution...", 1)
-report("Elapsed time: $(tEnd - tStart)")
+# Write out full solution
+writeSolution(t, x, y, z, Q, input.nVars)
+
+# TODO: write out a slice at any x-y, x-z, or y-z plane
--- src/file_io.jl
+++ src/file_io.jl
@@ -30,6 +30,40 @@ function readSettings(filename::String)
 
 end
 
+# Function for writing the solution to a .vtr file
+function writeSolution(t::Float64, x::Array{Float32,3}, y::Array{Float32,3}, z::Array{Float32,3}, Q::Array{Float32,4}, iNVars::Int64)
+    # Check that there are 8, 10 or 12 variables
+    if (iNVars == 8) || (iNVars == 10) || (iNVars == 12)
+        # Write solution to a vtr file
+        filename = "data/solution_$(rpad(string(round(t, digits=5)), 7, "0")).vtr"
+        tStart = report("Writing the solution at time $(rpad(string(round(t, digits=5)), 7, "0"))", 1)
+        WriteVTK.vtk_grid(filename, x[:, 1, 1], y[1, :, 1], z[1, 1, :]) do vtk
+            vtk["MomentumX"] = Q[:, :, :, 1]
+            vtk["MomentumY"] = Q[:, :, :, 2]
+            vtk["MomentumZ"] = Q[:, :, :, 3]
+            vtk["EnergyDensity"] = Q[:, :, :, 4]
+            if (iNVars >= 10)
+                for v = 1:2
+                    vtk["DensityMassFraction$(v)"] = Q[:, :, :, 4+v]
+                end
+            end
+            if (iNVars == 12)
+                for v = 1:2
+                    vtk["VolumeFraction$(v)"] = Q[:, :, :, 4+2+v]
+                end
+            end
+            vtk["Density"] = Q[:, :, :, iNVars-3]
+            vtk["Gamma"] = Q[:, :, :, iNVars-2]
+            vtk["Pressure"] = Q[:, :, :, iNVars-1]
+            vtk["Temperature"] = Q[:, :, :, iNVars]
+        end
+    else
+        error("Number of variables must be 8, 10 or 12")
+    end
+    tEnd = report("Finished writing solution...", 1)
+    report("Elapsed time: $(tEnd - tStart)")
+end
+
 # Function for reading a Plot3D grid file
 function readPlot3DGrid(timeStep::String, Nx::Int64, Ny::Int64, Nz::Int64)
     # Read prempi.dat file
