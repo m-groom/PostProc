788b3cad20a1f3b013da4e9b3d63cbd528429dc2
michael <michael.groom@sydney.edu.au>
2023-05-10 18:19:46 +1000

    Ccalculating spectra with correct normalisation

--- src/spectral_quantities.jl
+++ src/spectral_quantities.jl
@@ -8,33 +8,33 @@ function calcSpectralQuantities(t::Float64, x::Array{Float32,3}, y::Array{Float3
     tEnd = report("Finished calculating radial power spectra...", 1)
     report("Elapsed time: $(tEnd - tStart)")
     # Write energy spectra to file
-    writeEnergySpectra(t, κ, Ex, Ey, Ez)
-    # Plot energy spectra
-    # plt.figure(1)
-    # plt.loglog(κ, Ex, label="u'")
-    # plt.loglog(κ, Ey, label="v'")
-    # plt.loglog(κ, Ez, label="w'")
-    # plt.xlabel("κ")
-    # plt.ylabel("E(κ)")
-    # plt.legend()
-    # plt.savefig("energy_spectra.png")
+    writeEnergySpectra(t, κ, Ex, Ey, Ez) # TODO: add noise compensation factor when writing spectra
+    # TODO calcualte integral length
+    # E = sum(E1Dy .+ E1Dz)*Δκ
 end
 
 # Function to calculate radial power spectra at x = x0 for each velocity component
 function calcPowerSpectra(t::Float64, x::Array{Float32,1}, y::Array{Float32,1}, z::Array{Float32,1}, Q::Array{Float32,4}, QBar::planeAverage, grid::rectilinearGrid, nVars::Int64, x0::Float64)
-    # Get wavenumbers
+    # Get wavenumbers and spacing
     κy = collect(FFTW.fftshift(FFTW.fftfreq(grid.Ny, grid.Ny)))
     κz = collect(FFTW.fftshift(FFTW.fftfreq(grid.Nz, grid.Nz)))
+    Ly = grid.yR - grid.yL
+    Lz = grid.zR - grid.zL
+    Δκy = 2.0 * pi / Ly
+    Δκz = 2.0 * pi / Lz
+    Δκ = max(Δκy, Δκz)
+    Δy = Ly / grid.Ny
+    Δz = Lz / grid.Nz
     # Calculatate maximum radial wavenumber
-    κMax = Int(ceil(sqrt(κy[1]^2 + κz[1]^2)))
+    κMax = Int(ceil(sqrt(2)*max(grid.Ny/2, grid.Nz/2)))
     # Initialise arrays
     uPrime = zeros(Float64, grid.Ny, grid.Nz) # u'
     vPrime = zeros(Float64, grid.Ny, grid.Nz) # v'
     wPrime = zeros(Float64, grid.Ny, grid.Nz) # w'
-    κ1D = collect(0:κMax) # radial wavenumber
     E1Dx = zeros(Float64, κMax + 1) # Energy spectrum for u' at x = x0
     E1Dy = zeros(Float64, κMax + 1) # Energy spectrum for v' at x = x0
     E1Dz = zeros(Float64, κMax + 1) # Energy spectrum for w' at x = x0
+    κ1D = collect(0:κMax) # radial wavenumber
     # Get location of interface
     i = argmin(abs.(x .- x0))
     # Get fluctuating velocity components
@@ -46,35 +46,27 @@ function calcPowerSpectra(t::Float64, x::Array{Float32,1}, y::Array{Float32,1}, 
             wPrime[j, k] = Q[i, j, k, 3] * rhoInv - QBar.WBar[i]
         end
     end
-    # 2D FFT
+    # 2D FFT to get power spectral density
     uHat = FFTW.fftshift(FFTW.fft(uPrime))
     vHat = FFTW.fftshift(FFTW.fft(vPrime))
     wHat = FFTW.fftshift(FFTW.fft(wPrime))
-    E2Dx = conj(uHat) .* uHat
-    E2Dy = conj(vHat) .* vHat
-    E2Dz = conj(wHat) .* wHat
-    # Calculate power spectra
-    num = zeros(Float64, κMax + 1)
+    E2Dx = uHat .* conj(uHat)
+    E2Dy = vHat .* conj(vHat)
+    E2Dz = wHat .* conj(wHat)
+    # Calculate energy spectra
+    constant = Δy * Δz * min(Δκy, Δκz) / (8.0 * pi^2 * grid.Ny * grid.Nz)
     for n in eachindex(κz)
         for m in eachindex(κy)
             κ = sqrt(κy[m]^2 + κz[n]^2)
-            idx = Int(ceil(κ)) + 1
-            E1Dx[idx] += E2Dx[m, n]
-            E1Dy[idx] += E2Dy[m, n]
-            E1Dz[idx] += E2Dz[m, n]
-            num[idx] += 1
+            for p = 0:κMax
+                κp = p * Δκ        
+                if (κp - Δκ/2 <= κ && κ < κp + Δκ/2)
+                    E1Dx[p+1] = E1Dx[p+1] + E2Dx[m, n] * constant
+                    E1Dy[p+1] = E1Dy[p+1] + E2Dy[m, n] * constant
+                    E1Dz[p+1] = E1Dz[p+1] + E2Dz[m, n] * constant
+                end
+            end
         end
-    end
-    # Normalise
-    for κ = 0:κMax
-        idx = κ + 1
-        if (num[idx] == 0)
-            num[idx] = 1
-        end
-        area = 0.25 * pi * ((κ + 0.5)^2 - (κ - 0.5)^2)
-        E1Dx[idx] = E1Dx[idx] * area / num[idx]
-        E1Dy[idx] = E1Dy[idx] * area / num[idx]
-        E1Dz[idx] = E1Dz[idx] * area / num[idx]
     end
 
     return E1Dx, E1Dy, E1Dz, κ1D
