5c6b566c2cc8334773468a02fbe7275bd680fb08
Michael Groom <michael.groom@sydney.edu.au>
2023-05-22 15:02:23 +1000

    Reverted to @batch in getPlaneAverages

--- src/plane_averages.jl
+++ src/plane_averages.jl
@@ -22,49 +22,27 @@ function getPlaneAverages(x::SubArray{Float32,1}, Q::Array{Float32,4}, Nx::Int64
     nPtsInv = 1.0 / (Ny * Nz)
     # Loop over all cells
     @inbounds begin
-        @tturbo for k = 1:Nz
-            for j = 1:Ny
-                for i = 1:Nx
+        @batch for i = 1:Nx
+            for k = 1:Nz
+                for j = 1:Ny
                     rhoBar[i] += Q[i, j, k, nVars-3]
                     UBar[i] += Q[i, j, k, 1]
-                end
-            end
-        end
-        if (nVars >= 10)
-            @tturbo for k = 1:Nz
-                for j = 1:Ny
-                    for i = 1:Nx
+                    if (nVars >= 10)
                         Y1Bar[i] += Q[i, j, k, 5]
                         muBar[i] += 1.0 / (Q[i, j, k, 5] / μ[1] + (1.0 - Q[i, j, k, 5]) / μ[2])
                         nuBar[i] += 1.0 / (Q[i, j, k, nVars-3] * (Q[i, j, k, 5] / μ[1] + (1.0 - Q[i, j, k, 5]) / μ[2]))
                     end
-                end 
-            end
-        end
-        if (nVars == 12)
-            @tturbo for k = 1:Nz
-                for j = 1:Ny
-                    for i = 1:Nx
+                    if (nVars == 12)
                         Z1Bar[i] += Q[i, j, k, 7]
-                        Z1Z2Bar[i] += Q[i, j, k, 7] * Q[i, j, k, 8]
-                    end
-                end
-            end
-        elseif (nVars == 10)
-            @tturbo for k = 1:Nz
-                for j = 1:Ny
-                    for i = 1:Nx
+                        Z1Z2Bar[i] += Q[i, j, k, 8]
+                    elseif (nVars == 10)
                         Z1 = R[1] * Q[i, j, k, 5] / (R[1] * Q[i, j, k, 5] + R[2] * (1.0 - Q[i, j, k, 5]))
                         Z1Bar[i] += Z1
                         Z1Z2Bar[i] += Z1 * (1.0 - Z1)
                     end
                 end
             end
-        end
-    end
-    # Divide by number of points to get averages
-    @inbounds begin
-        @turbo for i = 1:Nx
+            # Divide by number of points to get averages
             rhoBar[i] *= nPtsInv
             UBar[i] *= nPtsInv
             Y1Bar[i] *= nPtsInv
